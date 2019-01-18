369fee57e4e682b9fd8083ce4fdb0596439beda8
Keesun Baik <whiteship2000@gmail.com>
2019-01-18 13:19:33 -0800

    First commit

---
+++ src/main/java/me/whiteship/demowebmvc/DemoWebMvcApplication.java
@@ -0,0 +1,14 @@
+package me.whiteship.demowebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebMvcApplication.class, args);
+    }
+
+}
+
