fbb37cca93303de5755bace2178925eecf047911
Keesun Baik <whiteship2000@gmail.com>
2019-01-22 15:18:00 -0800

    @SessionAttributes

--- src/main/java/me/whiteship/demowebmvc/SampleController.java
+++ src/main/java/me/whiteship/demowebmvc/SampleController.java
@@ -5,12 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@SessionAttributes("event")
 public class SampleController {
 
     @GetMapping("/events/form")
@@ -23,11 +26,12 @@ public class SampleController {
 
     @PostMapping("/events")
     public String createEvent(@Validated @ModelAttribute Event event,
-                           BindingResult bindingResult) {
+                              BindingResult bindingResult,
+                              SessionStatus sessionStatus) {
         if(bindingResult.hasErrors()) {
             return "/events/form";
         }
-
+        sessionStatus.setComplete();
         return "redirect:/events/list";
     }
 
--- src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
+++ src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
 
+import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -25,11 +27,14 @@ public class SampleControllerTest {
 
     @Test
     public void eventForm() throws Exception {
-        mockMvc.perform(get("/events/form"))
+        MockHttpServletRequest request = mockMvc.perform(get("/events/form"))
                 .andDo(print())
                 .andExpect(view().name("/events/form"))
                 .andExpect(model().attributeExists("event"))
-        ;
+                .andExpect(request().sessionAttribute("event", notNullValue()))
+                .andReturn().getRequest();
+        Object event = request.getSession().getAttribute("event");
+        System.out.println(event);
     }
 
 
