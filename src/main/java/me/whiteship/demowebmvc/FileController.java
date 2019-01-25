7a3b6fb047694a4745b1bf972d184c86054a2802
Keesun Baik <whiteship2000@gmail.com>
2019-01-24 20:33:33 -0800

    File Download

--- src/main/java/me/whiteship/demowebmvc/FileController.java
+++ src/main/java/me/whiteship/demowebmvc/FileController.java
@@ -1,16 +1,25 @@
 package me.whiteship.demowebmvc;
 
+import org.apache.tika.Tika;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.io.IOException;
+
 @Controller
 public class FileController {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     @GetMapping("/file")
     public String fileUploadForm(Model model) {
@@ -26,6 +35,21 @@ public class FileController {
         String message = file.getOriginalFilename() + " is uploaded";
         attributes.addFlashAttribute("message", message);
         return "redirect:/file";
+    }
+
+    @GetMapping("/file/{filename}")
+    public ResponseEntity<Resource> fileDownload(@PathVariable String filename) throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:" + filename);
+        File file = resource.getFile();
+
+        Tika tika = new Tika();
+        String mediaType = tika.detect(file);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachement; filename=\"" + resource.getFilename() + "\"")
+                .header(HttpHeaders.CONTENT_TYPE, mediaType)
+                .header(HttpHeaders.CONTENT_LENGTH, file.length() + "")
+                .body(resource);
     }
 
 }
